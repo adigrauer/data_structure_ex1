2dba52eeaab7d152fa1704d54ee81dba530520cd
lottemarava <lottemarava@gmail.com>
2022-04-16 12:23:10 +0300

    first update

---
+++ TreeNode.h
@@ -0,0 +1,23 @@
+#ifndef TREENODE_H
+#define TREENODE_H
+
+#include<iostream>
+#include<memory>
+
+using std::shared_ptr;
+
+template<class T>
+class TreeNode {
+    private:
+        shared_ptr<TreeNode<T>> left_node;
+        shared_ptr<TreeNode<T>> right_node;
+        int height;
+        shared_ptr<T> data;
+        //shared_ptr<TreeNode<T>> father;
+    public:
+
+}
+
+
+
+#endif /*TREENODE_H*/
---
+++ tree.h
@@ -0,0 +1,36 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "TreeNode.h"
+#include<iostream>
+#include<memory>
+
+using std::shared_ptr;
+
+template<class T>
+class Tree {
+    public:
+        Tree ();
+        
+        /* Tree copy C'tor */
+        Tree (const Tree& Tree) = default;
+        
+        /* Tree virtual D'tor */
+        ~Tree() = default;
+        TreeNode<T>* insert (TreeNode<T>* root, T to_add);
+        TreeNode<T>* remove(TreeNode<T>* root, T to_remove);
+        TreeNode<T> *rrRotate (TreeNode<T>* root); 
+        TreeNode<T> *lrRotate (TreeNode<T>* root);
+        TreeNode<T> *rlRotate (TreeNode<T>* root);
+        TreeNode<T> *llRotate (TreeNode<T>* root);
+        int max(int hight_a, int hight_b);
+        void inOrder (TreeNode<T>* root);
+        void reverseInOrder (TreeNode<T>* root);
+        TreeNode<T> *merge(TreeNode<T>* root_a, TreeNode<T>* root_b);
+        TreeNode<T> *find (TreeNode<T>* root, T to_find);
+        TreeNode<T> *checkBalance (TreeNode<T>* root);
+}
+
+
+
+#endif /*TREE_H*/
